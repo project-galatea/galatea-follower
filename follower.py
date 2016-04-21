91770bbbbc2e3d86225ad296d0b0c2e3d550496b
nacgarg <nachiketa.gargi@gmail.com>
2016-04-20 23:56:10 -0700

    made handleConn part of Follower class

--- follower.py
+++ follower.py
@@ -14,6 +14,8 @@ class Follower():
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        self.chatDict = dict()
+
         self.load_nn()
 
         self.socket.bind((self.host, self.port))
@@ -23,41 +25,40 @@ class Follower():
         while 1:
             conn, addr = self.socket.accept()
 
-            start_new_thread(handleConn ,(conn,))
+            start_new_thread(self.handleConn ,(conn,))
 
         self.socket.close()
 
     def load_nn(self):
         self.g = Galatea()
 
-chatDict = dict()
 
-def handleConn(conn):
+    def handleConn(self, conn):
 
-    while True:
+        while True:
 
-        #Receiving from client
-        data = conn.recv(4096)
-        if not data:
-            break
+            #Receiving from client
+            data = conn.recv(4096)
+            if not data:
+                break
 
-        msg = ipcMessage_pb2.Message()
-        msg.ParseFromString(data)
+            msg = ipcMessage_pb2.Message()
+            msg.ParseFromString(data)
 
-        if msg.chatId not in chatDict:
-            chatDict[msg.chatId] = chat.Chat()
+            if msg.chatId not in chatDict:
+                chatDict[msg.chatId] = chat.Chat()
 
-        chatDict[msg.chatId].addMessage(msg)
+            chatDict[msg.chatId].addMessage(msg)
 
-        respMsg = ipcMessage_pb2.Message()
-        respMsg.text = chatDict[msg.chatId].runNN()
-        respMsg.chatId = msg.chatId
-        respMsg.userId = msg.userId
+            respMsg = ipcMessage_pb2.Message()
+            respMsg.text = chatDict[msg.chatId].runNN()
+            respMsg.chatId = msg.chatId
+            respMsg.userId = msg.userId
 
-        conn.send(respMsg.SerializeToString())
+            conn.send(respMsg.SerializeToString())
 
 
-    conn.close()
+        conn.close()
 
 if __name__ == '__main__':
     Follower(24833)
